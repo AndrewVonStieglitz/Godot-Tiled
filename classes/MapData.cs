5b61947a3924bd1694c44727da8637ff233da76c
Andrew Von Stieglitz <49591230+AndrewVonStieglitz@users.noreply.github.com>
2023-03-14 16:25:56 +1100

    first commit

---
+++ classes/MapData.cs
@@ -0,0 +1,65 @@
+using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json;
+
+public class MapData
+{
+	public int compressionlevel { get; set; }
+	public int height { get; set; }
+	public bool infinite { get; set; }
+	public List<Layer> layers { get; set; }
+	public int nextlayerid { get; set; }
+	public int nextobjectid { get; set; }
+	public string orientation { get; set; }
+	public string renderorder { get; set; }
+	public string tiledversion { get; set; }
+	public int tileheight { get; set; }
+	public List<object> tilesets { get; set; }
+	public int tilewidth { get; set; }
+	public string type { get; set; }
+	public string version { get; set; }
+	public int width { get; set; }
+
+	public static MapData Load(string jsonPath)
+	{
+		using (StreamReader reader = new StreamReader(jsonPath))
+		{
+			string jsonString = reader.ReadToEnd();
+			return JsonConvert.DeserializeObject<MapData>(jsonString);
+		}
+	}
+}
+
+public class Layer
+{
+	public string draworder { get; set; }
+	public int id { get; set; }
+	public string name { get; set; }
+	public List<TiledObject> objects { get; set; }
+	public float opacity { get; set; }
+	public string type { get; set; }
+	public bool visible { get; set; }
+	public int x { get; set; }
+	public int y { get; set; }
+}
+
+public class TiledObject
+{
+	public string @class { get; set; }
+	public bool ellipse { get; set; }
+	public int height { get; set; }
+	public int id { get; set; }
+	public string name { get; set; }
+	public List<Point> polygon { get; set; }
+	public int rotation { get; set; }
+	public bool visible { get; set; }
+	public int width { get; set; }
+	public int x { get; set; }
+	public int y { get; set; }
+}
+
+public class Point
+{
+	public int x { get; set; }
+	public int y { get; set; }
+}
---
+++ scripts/CreateWorld.cs
@@ -0,0 +1,63 @@
+using Godot;
+using System;
+
+public partial class CreateWorld : Node
+{
+	MapData mapData = MapData.Load("map/world_map.json");
+
+	public override void _Ready()
+	{
+		foreach (Layer layer in mapData.layers)
+		{
+			if (layer.type == "objectgroup")
+			{
+				foreach (TiledObject @object in layer.objects)
+				{
+					var node = new Node2D();
+					node.Position = new Vector2(@object.x, @object.y);
+					AddChild(node);
+
+					var staticBody = new StaticBody2D();
+					staticBody.Position = new Vector2(@object.width / 2, @object.height / 2);
+					node.AddChild(staticBody);
+
+					if (@object.ellipse)
+					{
+						var circleShape = new CircleShape2D();
+						circleShape.Radius = @object.width / 2;
+						var collisionShape = new CollisionShape2D();
+						collisionShape.Shape = circleShape;
+						staticBody.AddChild(collisionShape);
+					}
+					else if (@object.polygon != null)
+					{
+						var collisionPolygon = new CollisionPolygon2D();
+						var points = new Vector2[@object.polygon.Count];
+						for (int i = 0; i < @object.polygon.Count; i++)
+						{
+							points[i] = new Vector2(@object.polygon[i].x, @object.polygon[i].y);
+						}
+						collisionPolygon.Polygon = points;
+						staticBody.AddChild(collisionPolygon);
+					}
+					else
+					{
+						var collisionShape = new CollisionShape2D();
+						var rectangleShape = new RectangleShape2D();
+						rectangleShape.Size = new Vector2(@object.width, @object.height);
+						collisionShape.Shape = rectangleShape;
+						staticBody.AddChild(collisionShape);
+					}
+				}
+
+
+
+			}
+		}
+	}
+
+	public override void _Process(double delta)
+	{
+	}
+
+}
